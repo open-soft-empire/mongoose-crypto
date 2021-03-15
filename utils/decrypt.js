ca8a46c92144d85151a1a1178d82b23b18d80b8b
votaryvirile <vimal.r.100@gmail.com>
2021-03-15 21:25:36 +0530

    Update decrypt.js

--- utils/decrypt.js
+++ utils/decrypt.js
@@ -1,6 +1,6 @@
 const crypto =require('crypto');
 
-const SymDeciph = (cipher, encryption_key, cipher_algorithm, digestion_module) => {
+const SymDeciph = (cipher, encryption_key, cipher_algorithm, digestion_module, type) => {
 	if(cipher) {
 		const iv_push = cipher.slice(0, 8);
 		const encrypted_shift = cipher.slice(8, 24);
@@ -12,14 +12,23 @@ const SymDeciph = (cipher, encryption_key, cipher_algorithm, digestion_module) =
 		const data = encrypted_shift + encrypted_garbage;
 
 		const iv = Buffer.from(iv_value, digestion_module);
+
 		const encryptedText = Buffer.from(data, digestion_module);
 		const decipher = crypto.createDecipheriv(cipher_algorithm, Buffer.from(encryption_key), iv);
 		
 		let decrypted = decipher.update(encryptedText);
-		decrypted = Buffer.concat([decrypted, decipher.final()]);
-		decrypted = JSON.parse(decrypted);
 
-		return decrypted;
+		decrypted = Buffer.concat([decrypted, decipher.final()]);
+
+		let decrypted_val;
+
+		if (type === 'field') {
+			decrypted_val = decrypted;
+		} else {
+			decrypted_val = JSON.parse(decrypted);
+		}
+
+		return decrypted_val;
 	} else {
 		return;
 	}
